c901c6bb7af75da3e30fbb90505830bc4d09739f
woutp <wjc.poelen@student.han.nl>
2022-01-11 13:09:21 +0100

    dag 2: Het bestand wordt geopend en als het goed is skippt de computer de eerste 40 regels. Ook split hij nu op de /.

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 #Wout Poelen
 
+def open_bestand():
+    """Deze functie opent het bestand"""
+    counter = 0
+
+    Genbank_file = open("GCF_000013425.1_ASM1342v1_genomic.gbff", "r")
+    reading = str(Genbank_file.readlines())
+
+    for read in reading:
+        counter += 1
+
+        if counter < 41:
+            pass
+
+    Genbank_split = reading.split("/")
+    print(Genbank_split)
+
+
 
 
 
 
 
 def main():
-    pass
+    open_bestand()
+
 
 main()
